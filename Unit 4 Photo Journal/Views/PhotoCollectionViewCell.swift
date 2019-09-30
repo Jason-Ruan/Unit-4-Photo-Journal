c04c5d1515a4e6c9277d971d624bfca8d89016a2
Jason Ruan <jasonruan@pursuit.org>
2019-09-29 22:11:43 -0400

    Moves files to folders

---
+++ Unit 4 Photo Journal/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Unit 4 Photo Journal
+//
+//  Created by Jason Ruan on 9/29/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
