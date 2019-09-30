c3cc05be1a775fc5070c1e091c7623cb5c53e05c
Jason Ruan <jasonruan@pursuit.org>
2019-09-29 22:01:59 -0400

    Adds library objects to initial view controller on main.storyboard

---
+++ Unit 4 Photo Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit 4 Photo Journal
+//
+//  Created by Jason Ruan on 9/29/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
