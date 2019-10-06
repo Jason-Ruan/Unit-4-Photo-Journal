ba0c6aa89af0b02ce84f23dbabdb469747621095
Jason Ruan <jasonruan@pursuit.org>
2019-10-06 19:07:05 -0400

    Adds new vc file and protocol file

---
+++ Unit 4 Photo Journal/View Controllers/NewEntryGalleryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewEntryGalleryViewController.swift
+//  Unit 4 Photo Journal
+//
+//  Created by Jason Ruan on 9/30/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class NewEntryGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
