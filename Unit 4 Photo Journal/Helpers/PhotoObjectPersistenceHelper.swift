92c5435343ac3777c3311052b9eae0e3417985a2
Jason Ruan <jasonruan@pursuit.org>
2019-10-06 22:34:05 -0400

    Defines PhotoPersistenceHelper

--- Unit 4 Photo Journal/Helpers/PhotoObjectPersistenceHelper.swift
+++ Unit 4 Photo Journal/Helpers/PhotoObjectPersistenceHelper.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct PhotoObjectPersistenceHelper {
+    static let manager = PhotoObjectPersistenceHelper()
+
+    func save(newPhotoObject: PhotoObject) throws {
+        try persistenceHelper.save(newElement: newPhotoObject)
+    }
+
+    func delete(photoID: Int) throws {
+        var elements = try getAlbum()
+        let newElements = elements.filter { $0.id != photoID }
+        let serializedData = try PropertyListEncoder().encode(newElements)
+        try serializedData.write(to: persistenceHelper.url, options: Data.WritingOptions.atomic)
+    }
+    
+    func getAlbum() throws -> [PhotoObject] {
+        return try persistenceHelper.getObjects()
+    }
+
+    private let persistenceHelper = PersistenceHelper<PhotoObject>(fileName: "photoObjects.plist")
+
+    private init() {}
+}
