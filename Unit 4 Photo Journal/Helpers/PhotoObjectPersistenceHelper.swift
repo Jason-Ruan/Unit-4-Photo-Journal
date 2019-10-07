ba2776f0f262ebfad00c71ff3a04389639875f41
Jason Ruan <jasonruan@pursuit.org>
2019-10-06 22:33:14 -0400

    Adds PhotoObject model, adds PhotoObjectPersistenceHelper and PersistenceHelper

---
+++ Unit 4 Photo Journal/Helpers/PhotoObjectPersistenceHelper.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoObjectPersistenceHelper.swift
+//  Unit 4 Photo Journal
+//
+//  Created by Jason Ruan on 10/6/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
---
+++ Unit 4 Photo Journal/Models/PhotoObject.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoObject.swift
+//  Unit 4 Photo Journal
+//
+//  Created by Jason Ruan on 10/6/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
